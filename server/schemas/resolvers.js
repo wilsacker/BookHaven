72904ce24c8563e3a540f0113b6ac6a158d2fccb
wilsacker <williamsuttona@gmail.com>
2024-09-30 17:44:26 -0700

    reslovers, typedefs bugs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,4 @@
-const User = require('./models/User'); // Adjust the import based on your actual user model
+const User = require('../models/User'); // Adjust the import based on your actual user model
 
 const resolvers = {
   Query: {
