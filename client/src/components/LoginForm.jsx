d95a2ef75566635d26e00a5ff5e54127aa137ac5
wilsacker <williamsuttona@gmail.com>
2024-10-01 20:47:07 -0700

    looking for bug

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -30,13 +30,11 @@ const LoginForm = () => {
 
     try {
       const { data } = await loginUser({
-        variables: { email: userFormData.email, password: userFormData.password }, // Send email and password as variables
+        variables: { ...userFormData },
       });
 
-      const token = data.login.token; // Access token from the response
-      const user = data.login.user; // Access user from the response
-      console.log(user);
-      Auth.login(token); // Log the user in
+      console.log(data);
+      Auth.login(data.login.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true); // Show alert if there's an error
@@ -52,38 +50,48 @@ const LoginForm = () => {
   return (
     <>
       <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-        <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
+        <Alert
+          dismissible
+          onClose={() => setShowAlert(false)}
+          show={showAlert}
+          variant="danger"
+        >
           Something went wrong with your login credentials!
         </Alert>
         <Form.Group className='mb-3'>
-          <Form.Label htmlFor='email'>Email</Form.Label>
+          <Form.Label htmlFor="email">Email</Form.Label>
           <Form.Control
-            type='text'
-            placeholder='Your email'
-            name='email'
+            type="text"
+            placeholder="Your email"
+            name="email"
             onChange={handleInputChange}
             value={userFormData.email}
             required
           />
-          <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
+          <Form.Control.Feedback type="invalid">
+            Email is required!
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group className='mb-3'>
-          <Form.Label htmlFor='password'>Password</Form.Label>
+          <Form.Label htmlFor="password">Password</Form.Label>
           <Form.Control
-            type='password'
-            placeholder='Your password'
-            name='password'
+            type="password"
+            placeholder="Your password"
+            name="password"
             onChange={handleInputChange}
             value={userFormData.password}
             required
           />
-          <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
+          <Form.Control.Feedback type="invalid">
+            Password is required!
+          </Form.Control.Feedback>
         </Form.Group>
         <Button
           disabled={!(userFormData.email && userFormData.password)}
-          type='submit'
-          variant='success'>
+          type="submit"
+          variant="success"
+        >
           Submit
         </Button>
       </Form>
--- server/server.js
+++ server/server.js
@@ -1,26 +1,29 @@
 const express = require('express');
+const path = require('path');
+// Import the ApolloServer class
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
-const path = require('path');
 const { authMiddleware } = require('./utils/auth');
+
+// Import the two parts of a GraphQL schema
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
-const app = express();
-
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
+const app = express();
+
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
   await server.start();
-
+  
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
-
+  
   app.use('/graphql', expressMiddleware(server, {
     context: authMiddleware
   }));
@@ -42,4 +45,4 @@ const startApolloServer = async () => {
 };
 
 // Call the async function to start the server
-  startApolloServer();
+startApolloServer();
